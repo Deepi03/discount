933a1ab384ff8126a1d0f6222220684e9039fdd2
Deepika Malini Rajasekar <deepirajsekar03@gmail.com>
2022-11-07 11:05:36 +0200

    initialize project and add product model

---
+++ src/models/Product.ts
@@ -0,0 +1,23 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  normalPrice: string;
+  discountPriceForTimeOfYear: PriceForTimeOfYear;
+  discountPriceForPastSale: PriceForPastSale;
+  discountPriceForCustomer: PriceForCustomer;
+}
+type TimeOfYear = "june" | "november" | "december";
+
+type PriceForTimeOfYear = Record<TimeOfYear, string>;
+
+type PastSalesAmount =
+  | "10000-20000"
+  | "20001-30000"
+  | "30001-40000"
+  | "40001-50000";
+
+type PriceForPastSale = Record<PastSalesAmount, string>;
+
+type Customer = "gigantti" | "verkokauppa" | "prisma";
+type PriceForCustomer = Record<Customer, string>;
